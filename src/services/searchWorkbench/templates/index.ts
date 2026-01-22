d5d72aa3751ed0dd5ad5da63afaa4a455a626762
aashishGitHub <aashish123pqr@gmail.com>
2026-01-22 22:16:04 +0530

    Add index file to export query templates and related utility functions

---
+++ src/services/searchWorkbench/templates/index.ts
@@ -0,0 +1,10 @@
+export {
+  QUERY_TEMPLATES,
+  getTemplateById,
+  getTemplatesByCategory,
+  getTemplateCategories,
+  getTemplateAsString,
+  getGroupedTemplates,
+} from './QueryTemplates';
+
+export type { QueryTemplate, TemplateCategory } from './QueryTemplates';
